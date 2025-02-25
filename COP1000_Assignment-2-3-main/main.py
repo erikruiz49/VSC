6d9a7c04e1d77e1c76ebd88f8b1edc1840b93857
Erik Ruiz <erikyaina@gamil.com>
2025-02-25 11:17:37 -0500

    2.3+

--- COP1000_Assignment-2-3-main/main.py
+++ COP1000_Assignment-2-3-main/main.py
@@ -3,12 +3,14 @@
 itemName = "TV Stand"
 retailPrice = 325.00
 wholesalePrice = 200.00
+salePrise = retailPrice - (retailPrice * 0.25)
+saleProfit = salePrise - wholesalePrice
 
 # Write your assignment statements here for profit, salePrice, and saleProfit
 
 print("Item Name: " + itemName)
 print("Retail Price: $" + str(retailPrice))
 print("Wholesale Price: $" + str(wholesalePrice))
-print("Profit: $" + str(profit))
-print("Sale Price: $" + str(salePrice))
+print("Profit: $" + str(retailPrice - wholesalePrice))
+print("Sale Price: $" + str(salePrise))
 print("Sale Profit: $" + str(saleProfit))
