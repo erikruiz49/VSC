c250414e19ca5dc647546f6ea75bdc9c79a849b5
Erik Ruiz <erikyaina@gamil.com>
2025-02-26 15:25:32 -0500

    2.4+

--- COP1000_Assignment-2-4-main/main.py
+++ COP1000_Assignment-2-4-main/main.py
@@ -1,8 +1,19 @@
+# tax
+STATE_TAX = 0.065
+FEDERAL_TAX = 0.28
+EMP_SALARY = 0.025
+
 # input statements
-salary = float(input())
-numDependents = float(input())
+salary = float(input('Pls, enter your salary: '))
+numDependents = float(input('Pls, enter num of dependents: '))
 
 # calculate taxes here
+
+stateTaxNum = STATE_TAX * salary
+federalTaxNum = FEDERAL_TAX * salary
+dependentDeduction = (EMP_SALARY * numDependents) * salary
+totalWithholding = stateTaxNum + federalTaxNum + dependentDeduction
+takeHomePay = salary - totalWithholding
 
 # output statements
 print("Salary: $" + str(salary))
