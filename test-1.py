2bd74db75b39ab7a129f81036fee1f4826063136
Erik Ruiz <erikyaina@gamil.com>
2025-03-27 22:21:32 -0400

    4.1

--- test-1.py
+++ test-1.py
@@ -1,3 +1,12 @@
-value = int(input("Please, enter a numerical value: "))
-result = value * 10
-print(result)
+#funtion
+def checkInput(x):
+    if(not x.isnumeric()):
+        return (f'{x} is not a numerical value')
+    if(int(x) > 10 or int(x) < 1):
+        return (f'{x} is not a valid number')
+    return (f'The number is {x}')
+
+#input
+response = input('pls, enter a number between 1 and 10: ')
+
+print(checkInput(response))
