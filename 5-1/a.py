f3184aae56652a06cc815d6a174a28a54b102a9a
Erik Ruiz <erikyaina@gamil.com>
2025-04-14 00:15:06 -0400

    5-1+

--- 5-1/a.py
+++ 5-1/a.py
@@ -1,9 +1,8 @@
 a = 1
 b = 2
-c = 5
-
-while a < b:
+c = 5 
+while a < c:
     a = a + 1
     b = b + c
-
+    
 print(a, b, c)
---
+++ 5-1/d.py
@@ -0,0 +1,9 @@
+j = 2
+k = 5
+n = 9
+while j < k:
+    m = 6
+    while m < n:
+        print('Goodbye')
+        m = m + 1
+    j = j + 1
