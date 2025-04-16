2754455832ca4f5f2727e2c5417c301578b55ab3
Erik Ruiz <erikyaina@gamil.com>
2025-04-16 18:04:02 -0400

    db

--- test-1.py
+++ test-1.py
@@ -1,8 +1,12 @@
-list = ['bike','cars']
+#list = ['bike','cars']
+#
+#decition = input('bike or cars? ')
+#if list.__contains__(decition):
+#    print('great')
+#else:
+#    print('no bueno', '\n' 'caca')
 
-decition = input('bike or cars? ')
-if list.__contains__(decition):
-    print('great')
-else:
-    print('no bueno', '\n' 'caca')
+db = ['pepe', 'manolo', 'roberto']
 
+response = db.read()
+print(response)
